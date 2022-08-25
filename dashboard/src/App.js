3760fe853b3d0c26645224d0d0e86b68bfe9cc1f
Tim La <tlamarketing111@gmail.com>
2022-08-25 12:28:53 -0400

    styled app component & inserted TooltipComponent

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -6,7 +6,19 @@ import './App.css'
 
 const App = () => {
     return (
-        <h1 className="underline text-3xl">App</h1>
+        <div>
+            <BrowserRouter>
+                <div className="flex relative dark:bg-main-dark-bg">
+                    <div className="fixed right-4 bottom-4" style={{ zIndex: 1000 }}>
+                        <TooltipComponent content="Settings" position="Top">
+                            <button>
+                                <FiSettings />
+                            </button>
+                        </TooltipComponent>
+                    </div>
+                </div>
+            </BrowserRouter>
+        </div>
     )
 }
 

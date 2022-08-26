437c7d662fb8a81c69179d6c55be376adcc3d981
Tim La <tlamarketing111@gmail.com>
2022-08-26 11:07:33 -0400

    imported components & pages in app component

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -2,7 +2,10 @@ import React, { useEffect } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { FiSettings } from 'react-icons/fi';
 import { TooltipComponent } from '@syncfusion/ej2-react-popups';
-import './App.css'
+import './App.css';
+
+import { Navbar, Footer, Sidebar, ThemeSettings } from './components';
+import { Ecommerce, Orders, Calendar, Employees, Stacked, Pyramid, Customers, Kanban, Line, Area, Bar, Pie, Financial, ColorPicker, ColorMapping, Editor } from './pages';
 
 const App = () => {
     const activeMenu = true;

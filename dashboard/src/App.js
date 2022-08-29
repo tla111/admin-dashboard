0c9de30ee5bd65b48f64980c6e8d7f4611f81fa2
Tim La <tlamarketing111@gmail.com>
2022-08-29 19:16:57 -0400

    removed console log & extra activeMenu

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -9,9 +9,6 @@ import { Ecommerce, Orders, Calendar, Employees, Stacked, Pyramid, Customers, Ka
 
 const App = () => {
     const activeMenu = true;
-    // const activeMenu = false;
-    console.log(activeMenu);
-
 
     return (
         <div>

c63e85a8c836a6da84107328aaeb4f5d8c4db640
Tim La <tlamarketing111@gmail.com>
2022-09-12 09:39:53 -0400

    imported useStateContext for app component

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -7,8 +7,10 @@ import './App.css';
 import { Navbar, Footer, Sidebar, ThemeSettings } from './components';
 import { Ecommerce, Orders, Calendar, Employees, Stacked, Pyramid, Customers, Kanban, Line, Area, Bar, Pie, Financial, ColorPicker, ColorMapping, Editor } from './pages';
 
+import { useStateContext } from './contexts/ContextProvider';
+
 const App = () => {
-    const activeMenu = true;
+    const { activeMenu } = useStateContext();
 
     return (
         <div>

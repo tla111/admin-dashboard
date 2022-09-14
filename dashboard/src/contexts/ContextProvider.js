caa5f9162303980993c9cc9f050f41135cf30867
Tim La <tlamarketing111@gmail.com>
2022-09-14 09:33:31 -0400

    created screenSize state in contextProvider

--- dashboard/src/contexts/ContextProvider.js
+++ dashboard/src/contexts/ContextProvider.js
@@ -12,6 +12,7 @@ const initialState = {
 export const ContextProvider = ({ children }) => {
     const [activeMenu, setActiveMenu] = useState(true);
     const [isClicked, setIsClicked] = useState(initialState);
+    const [screenSize, setScreenSize] = useState(undefined);
 
     const handleClick = (clicked) => {
         setIsClicked({ ...initialState, [clicked]: true });
@@ -24,7 +25,9 @@ export const ContextProvider = ({ children }) => {
                 setActiveMenu: setActiveMenu,
                 isClicked: isClicked,
                 setIsClicked: setIsClicked,
-                handleClick: handleClick
+                handleClick: handleClick,
+                screenSize: screenSize,
+                setScreenSize: setScreenSize
             }}
         >
             {children}

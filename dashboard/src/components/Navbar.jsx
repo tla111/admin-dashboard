8b9eee925b87902091c71db1d2d1e7c24f768af7
Tim La <tlamarketing111@gmail.com>
2022-09-14 09:22:01 -0400

    shows avatar & username

--- dashboard/src/components/Navbar.jsx
+++ dashboard/src/components/Navbar.jsx
@@ -48,6 +48,19 @@ const Navbar = () => {
                     color="blue"
                     icon={<RiNotification3Line />}
                 />
+                <TooltipComponent content="Profile" position="BottomCenter">
+                    <div className="flex items-center gap-2 cursor-pointer p-1 hover:bg-light-gray rounded-lg" onClick={() => handleClick("userProfile")}>
+                        <img
+                            className="rounded-full w-8 h-8"
+                            src={avatar}
+                            alt="avatar"
+                        />
+                        <p>
+                            <span className="text-gray-400 text-14">Hi, </span> {" "}
+                            <span className="text-gray-400 font-bold ml-1">Michael</span>
+                        </p>
+                    </div>
+                </TooltipComponent>
             </div>
         </div>
     )

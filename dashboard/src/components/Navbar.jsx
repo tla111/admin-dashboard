dfef2f86443005eb0a0dce7c787d2f2f5d6045e6
Tim La <tlamarketing111@gmail.com>
2022-09-14 09:14:02 -0400

    used navButton component to create specific nav  menu button

--- dashboard/src/components/Navbar.jsx
+++ dashboard/src/components/Navbar.jsx
@@ -11,8 +11,10 @@ import { Cart, Chat, Notification, UserProfile } from '.';
 import { useStateContext } from '../contexts/ContextProvider';
 
 const NavButton = ({ title, customFunc, icon, color, dotColor }) => (
-    <TooltipComponent>
-
+    <TooltipComponent content={title} position="BottomCenter">
+        <button type="button" onClick={customFunc} style={{ color }} className="relative text-xl rounded-full p-3 hover:bg-light-gray">
+            <span style={{ background: dotColor }} className="absolute inline-flex rounded-full h-2 w-2 right-2 top-2">{icon}</span>
+        </button>
     </TooltipComponent>
 )
 
@@ -21,7 +23,7 @@ const Navbar = () => {
 
     return (
         <div className="flex justify-between p-2 md:mx-6 relative">
-
+            <NavButton title="Menu" customFunc={() => setActiveMenu((prevActiveMenu) => !prevActiveMenu)} color="blue" icon={<AiOutlineMenu />} />
         </div>
     )
 }
